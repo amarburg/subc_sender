aa344ca56d41480089c4e21d4a06ac29992054d1
Aaron Marburg <aaron@apl.washington.edu>
2020-09-29 16:39:08 -0700

    Working on sync_to_s3

--- sync_to_s3.py
+++ sync_to_s3.py
@@ -1,41 +1,130 @@
 #!/usr/bin/env python3
 
-import inotify.adapters
+import time
 import boto3
+from botocore.exceptions import ClientError
 from PIL import Image
 import logging
+from tempfile import TemporaryDirectory
+import subprocess
+import re
 
 from pathlib import Path
+from datetime import datetime, timedelta
 
-src_path = Path("/mnt/left_camera/Stills")
+from watchdog.observers.polling import PollingObserver
+from watchdog.events import FileSystemEventHandler
+
+left_paths = [ Path("/mnt/left_camera/Stills") ]
+
+right_path = Path("/mnt/right_camera/Stills")
+
+def dng_to_png( dngpath, pngpath ):
+
+    cmd = ["convert", dngpath, pngpath]
+    logging.debug(cmd)
+
+    convert_out = subprocess.run(cmd, capture_output=True)
+    logging.info(convert_out.stdout.decode())
+
+def subc_file_to_datetime( filename ):
+    ## Isolate just the file name itself
+    filename = filename.name
+
+    return datetime.strptime(filename, "%Y.%m.%d %H.%M.%S.%f.dng")
+
+class ImagePair:
+
+    def __init__(self,left):
+        self.left = left
+        self.right = None
+
+class SubcEventHandler( FileSystemEventHandler ):
 
 
-def _main():
 
-    i = inotify.adapters.Inotify()
-    i.add_watch( str(src_path) )
+    def on_created(self, event):
+        super(SubcEventHandler, self).on_created(event)
 
-    for event in i.event_gen(yield_nones=False):
-        (_, type_names, path, filename) = event
+        what = 'directory' if event.is_directory else 'file'
+        logging.info("Created %s: %s", what, event.src_path)
 
-        logging.debug("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format(
-              path, filename, type_names))
+        newfile = Path(event.src_path)
+        if newfile.suffix != ".dng":
+            return
+
+        left_date = subc_file_to_datetime( newfile )
+        logging.debug("   detected new left raw file %s with date %s" % (newfile, left_date) )
+
+        # Look for matching right image
+        ## restrict to images from today
+        right_glob = "%04d.%d.%d*.dng" % (left_date.year, left_date.month, left_date.day)
+        right_files = right_path.glob(right_glob)
+
+        pair = ImagePair(newfile)
+
+        for right_file in right_files:
+            logging.debug(right_file)
+
+            right_date = subc_file_to_datetime( right_file )
+
+            if abs(right_date - left_date) < timedelta(seconds=1):
+                logging.info("Found matching rightfile %s" %right_file)
+
+                pair.right = right_file
+                break
+
+        else:
+            logging.error( "Did not find matching right file")
 
 
-        if 'IN_CREATE' in type_names:
-            logging.info("File %s created in %s" % (filename, path) )
+
+        def convert_and_upload( file, label ):
+
+            #with TemporaryDirectory() as td:
+
+            dngfile = Path("/tmp") / Path(file.name)
+            shutil.copy( file, dngfile )
+
+            pngfile = dngfile.with_suffix(".png")
+
+            dng_to_png( dngfile, pngfile )
+
+            # Upload the file
+            s3_client = boto3.client('s3')
+            bucket = "nasa-invader-subc-cameras"
+            object_name = Path(label) / pngfile.name
 
             try:
-                image_file = Path(path) / filename
-                logging.debug("Checking file %s" % image_file)
-                with Image.open( str(image_file) ) as im:
-                    logging.info(infile, im.format, "%dx%d" % im.size, im.mode)
-            except OSError:
-                pass
+                response = s3_client.upload_file( str(pngfile), bucket, str(object_name) )
+            except ClientError as e:
+                logging.error(e)
+                return False
+            return True
+
+
+        convert_and_upload( pair.left, "left" )
+        if pair.right:
+            convert_and_upload( pair.right, "right" )
 
 
 if __name__ == '__main__':
 
     logging.basicConfig( level=logging.DEBUG )
 
-    _main()
+    event_handler = SubcEventHandler()
+
+    observer = PollingObserver()
+    for p in left_paths:
+        logging.debug("Watching %s" % p)
+        observer.schedule(event_handler, str(p) )
+
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
