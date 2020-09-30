90a4fe31839ca727c7eb204e50b170f3c2b145e9
Aaron Marburg <aaron@apl.washington.edu>
2020-09-29 22:48:52 -0700

    Removed sync_left_to_s3.sh

--- sync_left_to_s3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SRC_PATH=/mnt/left_camera/
-DST_PATH=/mnt/s3-nasa-invader-subc-cameras/left/
-
-while true; do
-  inotifywait -r -e modify,attrib,close_write,move,create,delete $SRC_PATH
-  rsync -avW --progress --inplace --size-only  $SRC_PATH $DST_PATH
-done
